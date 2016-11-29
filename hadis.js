bcd3c2f32fde560e498eb778e94bd7a5c390ee72
TheAdnan <adnankicin92@gmail.com>
2016-11-29 01:53:15 +0100

    initial commit

---
+++ hadis.js
@@ -0,0 +1,8 @@
+function hadisDana(){
+	var x = document.getElementsByClassName("aphorismhadis-dana")[0].innerText;
+	document.getElementById("gkHeaderNav").innerText = x;
+	document.getElementById("gkHeaderNav").style.fontSize = "50px";
+}
+
+
+hadisDana();
